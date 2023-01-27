a1bc57724c8b3d58fdb1f28f5c68defdfefa7921
Veerasravan <veerasravank@gmail.com>
2023-01-27 10:17:16 +0530

    created a stringMethod.js file.

---
+++ Basics/stringMethod.js
@@ -0,0 +1,56 @@
+//concatenating the string with '+' sign
+
+let outStr = "I am the first. ";
+
+let name = " Karthikeya..";
+let greet = "Hello" + name;
+console.log(greet);
+
+// concatenating withe += opeatator
+let myStr = "I come first. ";
+myStr += "I come second."
+console.log(myStr);
+
+// Constructing the strings with variables
+const ourName = "freeCodeCamp";
+const upStr = "Hello Our name is " + ourName + ", how are you";
+console.log(upStr);
+
+// Appending variables to Strings
+const anAdjective = "awesome!";
+let ourSt = "freeCodeCamp is: ";
+ourSt += anAdjective;
+console.log(ourSt);
+
+//WE can find the length of a string value by means .length();
+
+console.log("Alan Peter".length);
+
+const firstName = "ada";
+console.log(firstName);
+console.log(firstName.length);
+
+let lastName = "lovelace";
+lastNameLength = lastName.length;
+console.log(lastNameLength);
+
+//Use Bracket Notation to find the first character in a string
+
+const firstNme = "Charles";
+const firstLetter = firstNme[0];
+console.log(firstLetter);
+
+// Using the bracket Notation to find the last character in a string
+
+const fName = "Steve";
+const lName = fName[fName.length - 1];
+console.log(fName);
+console.log(lName);
+
+const fiName = "Augusta";
+console.log(fiName);
+console.log(fiName[fiName.length-3]);
+
+
+
+
